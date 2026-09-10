7a15432a55e79955c060bf7cce64b7e8c7e9f9d6
Annettaaaaa <87325258+Annettaaaaa@users.noreply.github.com>
2026-09-10 22:27:45 +0000

    Point mass min time optimisation. Trying to learn on a simpler models for now.

---
+++ Learning/point mass min time optimiser learning.py
@@ -0,0 +1,26 @@
+import casadi as ca
+import numpy as np
+
+# Problem parameters
+s_total = 100; # total distance to cross (m)
+m = 200; # mass (kg)
+
+F_max = 6000; # Max force applied during acceleration (N)
+F_min = - 6000; # Min force applied during braking (N)
+# Initial time
+
+# Final time
+
+# Input bounds
+
+
+# Bounds on the first control action
+
+
+# Input constraint error bounds
+
+# Bounds for path constraint function
+
+
+
+# Bounds for boundary constraints
